330b86437b481afac8dc0fe2ca7d1fe8f9aab7aa
Wqiang404 <707425493@qq.com>
2024-08-21 23:06:42 +0800

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import React from "react";
+import Layout from "./page/Layout";
+import Home from "./page/Home";
+import About from "./page/About";
+import Dashboard from "./page/Dashboard";
+import NoMatch from "./page/NoMatch";
 
-export default function App(){
-    return <div>
-        <h1>App</h1>
+
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
     </div>
+  );
 }
+
---
+++ src/page/Layout.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+function Layout() {
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/nothing-here">Nothing Here</Link>
+            </li>
+          </ul>
+        </nav>
+  
+        <hr />
+  
+        <Outlet />
+      </div>
+    );
+  }
+
+  export default Layout;
+  
