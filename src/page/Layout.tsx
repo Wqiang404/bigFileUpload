201e9dba190dfb6bfca90f81064bfbc89b96ac1b
Wqiang404 <707425493@qq.com>
2024-08-23 17:45:51 +0800

    layout调整

--- src/page/Layout.tsx
+++ src/page/Layout.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Menu } from 'antd'; 
+import '../static/css/index.css'
 
 function Layout() {
+    const menuItems = [  
+      { key: 'home', title: 'Home', path: '/' },  
+      { key: 'about', title: 'About', path: '/about' },  
+      { key: 'dashboard', title: 'Dashboard', path: '/dashboard' },  
+    ];
     return (
-      <div>
-        <nav>
+      <div className="layout">
+        {/* <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -19,11 +26,22 @@ function Layout() {
               <Link to="/nothing-here">Nothing Here</Link>
             </li>
           </ul>
-        </nav>
+        </nav> */}
+        <div className='menu'>
+          <Menu mode="vertical" style={{ lineHeight: '64px', height: '100%' }}>  
+            {menuItems.map(item => (  
+              <Menu.Item key={item.key}>  
+                <Link to={item.path}>{item.title}</Link>  
+              </Menu.Item>  
+            ))}  
+          </Menu>
+        </div>
   
-        <hr />
+        <div className="container">
+          <Outlet />
+        </div>
   
-        <Outlet />
+        
       </div>
     );
   }
